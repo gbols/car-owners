f6bfc842580328824f4344fc732e41ce8c345fea
gbols <gbolahanpaul@gmail.com>
2020-05-29 23:08:27 +0100

    setting up matser branch for testing

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ const envFound = dotenv.config();
 if (!process.env.NODE_ENV==='production' && envFound.error) {
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
-
+  
 export default {
   port: process.env.PORT,
   production: {
@@ -14,7 +14,7 @@ export default {
     databaseURI: process.env.DATABASE_URI_DEV,
   },
   test: {
-    databaseURI: process.env.DATABASE_URI_DEV,
+    databaseURI: process.env.DATABASE_URI_TEST,
   },
   apiPrefix: '/api/v1/'
 }
--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -4,6 +4,8 @@ import config from '../config';
 const environment = process.env.NODE_ENV || 'development';
 const dbString = config[environment];
 
+console.log({dbString, environment});
+
 export default async () => {
   const client = await mongoose.connect(dbString.databaseURI, { useNewUrlParser: true , useUnifiedTopology: true });
   const db = mongoose.connection;
